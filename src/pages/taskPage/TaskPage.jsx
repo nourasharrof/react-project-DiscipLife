e8058361a34a4cbd2b6d651c220a699554600f4d
nourasharrof <shanoura2@gnail.com>
2026-04-29 22:08:15 +0300

    fix taskContent import path

--- src/pages/taskPage/TaskPage.jsx
+++ src/pages/taskPage/TaskPage.jsx
@@ -1,8 +1,8 @@
-import TaskContent from "../../components/task/taskContent";
 import Header from "../../components/template/header/Header";
 import Main from "../../components/template/mainComponent/Main";
 import Sidbar from "../../components/template/sidbar/Sidbar";
 import Content from "../../components/template/content/Content";
+import TaskContent from "../../components/task/TaskContent";
 
 const TaskPage = () => {
   return (
