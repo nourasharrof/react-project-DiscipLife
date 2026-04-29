85788b500905b646dcedb8910fa5e0d1c00ab69f
nourasharrof <shanoura2@gnail.com>
2026-04-29 21:41:34 +0300

    update site

--- src/pages/expensesPage/ExpensesPage.jsx
+++ src/pages/expensesPage/ExpensesPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import ExpensesContent from "../../components/expenses/ExpensesContent/";
 import Header from "../../components/template/header/Header";
 import Main from "../../components/template/mainComponent/Main";
 import Sidbar from "../../components/template/sidbar/Sidbar";
 import Content from "../../components/template/content/Content";
+import ExpensesContent from "../../components/expenses/ExpensesContent";
 
 const ExpensesPage = () => {
   return (
